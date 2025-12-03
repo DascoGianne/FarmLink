a3926ecbbaa8502721ff34875adf8ef147554901
Arwind Calapao <arwinddude1@gmail.com>
2025-12-03 16:24:33 +0800

    updated

---
+++ client/public/js/Home Page - Buyer/Terms.js
@@ -0,0 +1,50 @@
+//loader
+window.onload = function() {
+    const loader = document.getElementById("loader");
+    const content = document.getElementById("content");
+
+    // Optional: wait a bit before starting animation
+    setTimeout(() => {
+        loader.classList.add("done");
+
+        // Wait for CSS transition to finish
+        setTimeout(() => {
+            loader.style.display = "none"; 
+            content.classList.add("show");
+        }, 600); 
+    }, 1000); // 1 second delay for demo
+};
+
+
+// Sidebar
+const sidebar = document.getElementById('sidebar');
+const sidebarBtn = document.querySelector('#top .left-group img');
+const closeBtn = document.querySelector('.sidebar-close');
+
+sidebarBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+});
+
+
+// Sidebar search functionality
+const searchInput = document.querySelector('.search-input');
+const menuItems = document.querySelectorAll('#sidebar-menu li');
+
+searchInput.addEventListener('input', function () {
+    let filter = searchInput.value.toLowerCase();
+
+    menuItems.forEach(item => {
+        let text = item.textContent.toLowerCase();
+
+        if (text.includes(filter)) {
+            item.style.display = "block";   
+        } else {
+            item.style.display = "none"; 
+        }
+    });
+});
+
--- client/public/js/Learn.js
+++ client/public/js/Learn.js
@@ -82,3 +82,36 @@ window.onload = function() {
 
   clampScroll();
 })();
+
+
+// Sidebar
+const sidebar = document.getElementById('sidebar');
+const sidebarBtn = document.querySelector('#top .left-group img');
+const closeBtn = document.querySelector('.sidebar-close');
+
+sidebarBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+});
+
+
+// Sidebar search functionality
+const searchInput = document.querySelector('.search-input');
+const menuItems = document.querySelectorAll('#sidebar-menu li');
+
+searchInput.addEventListener('input', function () {
+    let filter = searchInput.value.toLowerCase();
+
+    menuItems.forEach(item => {
+        let text = item.textContent.toLowerCase();
+
+        if (text.includes(filter)) {
+            item.style.display = "block";   
+        } else {
+            item.style.display = "none"; 
+        }
+    });
+});
